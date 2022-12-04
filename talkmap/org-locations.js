3e85e5c8eeb136898b48cb327ec7449ab8a3ab08
I Kit Cheng <matthewkit@gmail.com>
2022-12-04 23:53:34 +0000

    [UPDATE] Put talkmap python script and jupyter notebook into talks folder [FIX] Fix error by specifying user_agent for geopy.Nominatim [UPDATE] Update locations for talks

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,12 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "Athens, Greece",
+    37.9839412,
+    23.7283052
   ],
   [
     "London, UK",
     51.5073219,
-    -0.1276473
-  ],
-  [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
-  ],
-  [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    -0.1276474
   ]
 ];
